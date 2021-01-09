1af5469fbde7360a5fb6eb66b03a8d0ce78e764f
alex170485 <alex.170485@icloud.com>
2021-01-09 17:34:46 +0300

    completed first hometask

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -1,31 +1,24 @@
 import React from "react";
+import Message from "./Message";
 
 const messageData = {
     avatar: "https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg",
-    name: "Some Name",
-    message: "some text",
+    name: "Алекс",
+    message: "привет как у тебя сегодня дела? Чем занимаешься? что собираешься делать вечером? Какие планы на новый год? ",
     time: "22:00",
 };
 
 function HW1() {
     return (
         <div>
-            <hr/>
             homeworks 1
+            <Message
+                avatar={messageData.avatar}
+                name={messageData.name}
+                message={messageData.message}
+                time={messageData.time}
+            />
 
-            {/*should work (должно работать)*/}
-
-            {/*<Message*/}
-            {/*    avatar={messageData.avatar}*/}
-            {/*    name={messageData.name}*/}
-            {/*    message={messageData.message}*/}
-            {/*    time={messageData.time}*/}
-            {/*/>*/}
-
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeMessage/>*/}
-            <hr/>
         </div>
     );
 }
--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,9 +1,28 @@
 import React from "react";
+import s from './Message.module.css'
 
-function Message() {
+
+
+type messageBlockPropsType = {
+    avatar: string
+    name:string
+    message:string
+    time:string
+}
+
+
+function Message(props:messageBlockPropsType) {
     return (
-        <div>
+        <div className={s.message}>
+           <div className={s.avatarBlock}><img src={props.avatar} alt=""/></div>
+            <div className={s.messageBlock}>
+                <div className={s.name}>{props.name}</div>
+                <div className={s.textMessage}>{props.message}
+                    <div className={s.timeMessage}>{props.time}</div>
+                </div>
 
+
+            </div>
         </div>
     );
 }
